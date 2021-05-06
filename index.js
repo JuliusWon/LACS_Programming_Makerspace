ebdc6e0bd8facd66b9222ee528a7f912f27bd31e
JuliusWon <71460934+JuliusWon@users.noreply.github.com>
2021-05-06 14:14:46 -0400

    added files

---
+++ index.js
@@ -0,0 +1,12 @@
+function OpenStackOverflow(){
+    window.open("https://stackoverflow.com/")
+}
+function Openbugfixer(){
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+}
+function OpenGithub(){
+    window.open("https://github.com/")
+}
+function OpenURL(URL){
+    window.open(URL);
+}
